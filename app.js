fbd957ec34498eba7e60f57ba6c6bb11cd6c024f
abramson21 <56224786+abramson21@users.noreply.github.com>
2020-01-20 11:14:24 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.post('/signin', login);
-app.post('/signup', createUser);
+//app.post('/signup', createUser);
 app.use(auth);
 
 app.use((req, res, next) => {
